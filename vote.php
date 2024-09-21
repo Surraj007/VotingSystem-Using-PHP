e4bc7c11d0ea0b62e419a68c42de670930336707
Suraj Kumar <surajraj4747@gmail.com>
2024-09-21 17:24:01 +0530

    Update vote.php

--- vote.php
+++ vote.php
@@ -2,12 +2,9 @@
 require 'db.php'; 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-   
     $option_id = $_POST['option_id'];
-
     $stmt = $db->prepare("UPDATE options SET votes = votes + 1 WHERE id = :id");
     $stmt->execute(['id' => $option_id]);
-
     header("Location: results.php");
     exit();
 }
